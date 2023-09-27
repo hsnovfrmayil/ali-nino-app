80d36d04fb2ff6c899bf81e7c0a269c261295259
hsnovfrmayil <cerpeleng68@gmail.com>
2023-09-27 11:37:13 +0400

    new comment

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -37,3 +37,75 @@ class AuthService extends ChangeNotifier {
     return await firebaseAuth.signOut();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class AuthService extends ChangeNotifier {
+//   final firebaseAuth = FirebaseAuth.instance;
+//   final fireStore = FirebaseFirestore.instance;
+
+//   Future<UserCredential?> signInWithEmailandPassword(
+//       String email, String password) async {
+//     try {
+//       UserCredential userCredential = await firebaseAuth
+//           .signInWithEmailAndPassword(email: email, password: password);
+//       fireStore
+//           .collection("users")
+//           .doc(userCredential.user!.uid)
+//           .collection("account")
+//           .doc(userCredential.user!.email)
+//           .set({
+//         "uid": userCredential.user!.uid,
+//         "email": userCredential.user!.email,
+//       }, SetOptions(merge: true));
+//       // fireStore.collection("alluser").doc(email).set({
+//       //   "user": email,
+//       //   "imageLink":
+//       //       "https://i.pinimg.com/1200x/6d/26/e2/6d26e264e3c11ec2c8c9025c91279ba1.jpg",
+//       // });
+//       return userCredential;
+//     } on FirebaseAuthException catch (e) {
+//       throw Exception(e.code);
+//     }
+//   }
+
+//   Future<UserCredential?> signUpWithEmailandPassword(
+//       String email, String password) async {
+//     try {
+//       UserCredential userCredential = await firebaseAuth
+//           .createUserWithEmailAndPassword(email: email, password: password);
+//       fireStore
+//           .collection("users")
+//           .doc(userCredential.user!.uid)
+//           .collection("account")
+//           .doc(userCredential.user!.email)
+//           .set({
+//         "uid": userCredential.user!.uid,
+//         "email": userCredential.user!.email,
+//         "imageLink":
+//             "https://i.pinimg.com/1200x/6d/26/e2/6d26e264e3c11ec2c8c9025c91279ba1.jpg"
+//       });
+//       fireStore.collection("alluser").doc(email).set({
+//         "user": email,
+//         "imageLink":
+//             "https://i.pinimg.com/1200x/6d/26/e2/6d26e264e3c11ec2c8c9025c91279ba1.jpg",
+//       });
+//       return userCredential;
+//     } on FirebaseAuthException catch (e) {
+//       throw Exception(e.code);
+//     }
+//   }
+
+//   Future<void> signOut() async {
+//     return await firebaseAuth.signOut();
+//   }
+// }
